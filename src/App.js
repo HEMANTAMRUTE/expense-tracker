faa1d503402ef68351c5ca5c29ca7497ae4e0374
unknown <hemantamrute12345@gmail.com>
2024-05-29 22:25:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './App.css'
+import {Header }from './comp/Header.js'
+import {Balance} from './comp/Balance.js'
+import { IncomeExpenses} from './comp/IncomeExpenses.js'
+import {TransactionList} from './comp/TransactionList.js'
+import {AddTransaction} from './comp/AddTransaction.js'
+import { GlobalProvider } from './comp/GlobalState';
+
+function App() {
+
+  return (
+    
+    <GlobalProvider>
+     <Header/>
+     <div className='container'>
+      <Balance/>
+      <IncomeExpenses/>
+      <TransactionList/>
+      <AddTransaction/>
+
+
+      
+     </div>
+      
+    </GlobalProvider>
+  );
+}
+
+export default App;
